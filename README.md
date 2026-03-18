2270f16e78362a0870ad5daa69371ad0b38532e9
Brian Faggin Batista <brianfbatista@gmail.com>
2026-03-17 22:06:21 -0500

    read me updated

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
+# Badger Research
+
+Badger ReSearch is a website to connect with other researchers inside UW through connecting with their research interests. In the web-page, you can create a profile, and track the topics you are interested in. You can also find other UW researchers, and their preferred research tracks. Finally, a profile has a Whiteboard, an input system that allows users to create nodes of information and keep notes on their own research.
+
+The project will consist of a main page with a login system and a create an account button. After logging in, the user gets taken to their main page, with their account information, their research topics, and their whiteboard.
+
+I’m curious about how the fetching of the API will work, and how creating new accounts would work. My plan is to make some dummy accounts so that I can test out “connecting” to those accounts (a Search function inspired by the one in HW2).
+
+The scope of this project might change depending on the difficulties found along the road. The bare minimum I plan on executing is a page where you can add your own research interests, and search for users with matching interests. The maximum scope would encompass a website that has an account creator, a research matcher, and an interactive whiteboard that can be used to annotate research ideas.
+
+
+
 # React + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,31 @@
 import { useState } from 'react'
 import '../App.css'
-import { Button, Card } from 'react-bootstrap'
+import { Button, Card, Form } from 'react-bootstrap'
 
 export default function HomePage() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const [searchQuery, setSearchQuery] = useState("");
 
   return (
     <div className="w-100 h-100 d-flex justify-content-center align-items-center">
       <Card className='m-4 p-2'>
+        <Card.Header>
+          <h1>Badger Research</h1>
+        </Card.Header>
         <Card.Body className='text-center'>
-          <h1>Hello World!</h1>
-          <Button onClick={() => setCount(o => o + 1)}>Count: {count}</Button>
+          {/* <Button onClick={() => setCount(o => o + 1)}>Count: {count}</Button> */}
+          <Form>
+                <Form.Label htmlFor="searchBar">Name</Form.Label>
+                <Form.Control id="searchBar"
+                            placeholder="Search for a research interest..."
+                            value={searchQuery}
+                            onChange={(e) => {setSearchQuery(e.target.value), setCurrentPage(1)}}/>
+                <br />
+                <Button variant="danger" onClick={null}>Reset Search</Button>
+                <br />
+                {/* <p>There are <strong id="num-results">{filterStudents?.length}</strong> student(s) matching your search.</p> */}
+                <p>There are <strong id="num-results">{null}</strong> result(s) matching your search.</p>
+            </Form>
         </Card.Body>
       </Card>
     </div>
